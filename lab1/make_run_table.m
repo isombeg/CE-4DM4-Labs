fbfd9bdbcc33da3833333627ee7bbabce8095597
Vijay Patel <108.vijayp@gmail.com>
2022-09-29 15:05:57 -0400

    Add theorhetical probability for run table

--- lab1/make_run_table.m
+++ lab1/make_run_table.m
@@ -16,6 +16,14 @@ function runTable = make_run_table(bitStream, run_type, probs)
     for i = 1:24
         runTable(3, i) = runTable(2, i)/sum_of_all_observed_runs;
     end
+
+    % Probability of a 1 or a 0 is 50% each (flipping a coin). So the 
+    % probability of getting k number of 0s or 1s in a row is (0.5)^k 
+    
+    % row 4 
+    for i = 1:24
+        runTable(4, i) = 0.5^i;
+    end
 end
 
 function kRunCount = find_k_run_count(bitStream, k, run_type)
